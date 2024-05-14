7c1352501d63f38f4a381f74aea98c07f944aa99
cherotich <cherohellen14@gmail.com>
2024-05-14 12:50:18 -0700

    codeal

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,11 @@
 from vehicle import Vehicle
 
 class Car(Vehicle):
-    pass
+    def go(self):
+        """
+        Method to describe the sound a car makes when it goes.
+
+        Returns:
+            str: A string representing the sound of a car.
+        """
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
--- lib/vehicle.py
+++ lib/vehicle.py
@@ -1,2 +1,19 @@
 class Vehicle:
-    pass
+    
+    def __init__(self, wheel_size, wheel_number):
+        """
+        Initialize a Vehicle with a wheel size and number.
+
+        Args:
+            wheel_size (int): The size of the vehicle's wheels.
+            wheel_number (int): The number of wheels the vehicle has.
+        """
+        self.wheel_size = wheel_size
+        self.wheel_number = wheel_number
+
+    def go(self):
+        return "vrrrrrrrooom!"
+
+    def fill_up_tank(self):
+        return "filling up!"
+
